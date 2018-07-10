8e38e8831e67e08a6f560a192de493c241cf09a2
Jack Branch <jackbranch@Jacks-MacBook-Pro.local>
2018-07-10 16:27:54 +0100

    step 8

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,4 +1,10 @@
 
 class DockingStation
 
+  def release_bike
+    
+  end
+
 end
+
+#DockingStation.respond_to?(:release_bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,10 @@
 require 'DockingStation'
 
-describe "DockingStation" do
+describe 'DockingStation' do
+
+  #it { is_expected.to respond_to(:release_bike) }
+  it 'release bike' do
+     expect(DockingStation.new).to respond_to(:release_bike)
+  end
 
 end
